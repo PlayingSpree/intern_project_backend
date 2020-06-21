27520c9b361406c6be65a6e54b349355e31ce918
Natchapon Santiphiboon <winingame@hotmail.com>
2020-06-21 15:26:10 +0700

    Revert "Merge remote-tracking branch 'origin/develop' into develop"
    This reverts commit d403a9d022f52d931b25143d7ca48128f0759af4.

--- authapp/cors.py
+++
@@ -1,7 +0,0 @@
-from django.http import response
-
-
-class CorsMiddleware(object):
-    def process_response(self, req, resp):
-        response["Access-Control-Allow-Origin"] = "*"
-        return response
--- intern_project_backend/settings.py
+++ intern_project_backend/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'authapp.CorsMiddleware' # CORS
 ]
 
 ROOT_URLCONF = 'intern_project_backend.urls'
