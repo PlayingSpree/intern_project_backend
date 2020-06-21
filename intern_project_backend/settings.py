2922a7f61cb23ee49554402ed85bd86172d9f92a
Natchapon Santiphiboon <winingame@hotmail.com>
2020-06-21 15:08:27 +0700

    CORS

--- intern_project_backend/settings.py
+++ intern_project_backend/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.CorsMiddleware' # CORS
 ]
 
 ROOT_URLCONF = 'intern_project_backend.urls'
