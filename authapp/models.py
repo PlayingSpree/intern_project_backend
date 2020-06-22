8a2e3bfcedf28fbbf559fe5e1770c96523fd9e0a
Natchapon Santiphiboon <winingame@hotmail.com>
2020-06-22 17:36:49 +0700

    Phone not require

--- authapp/models.py
+++ authapp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(verbose_name='email', max_length=255, unique=True)
-    phone = models.CharField(blank=True, null=True, max_length=255)
-    REQUIRED_FIELDS = ['username', 'phone', 'first_name', 'last_name']
+    phone = models.CharField(null=True, max_length=255)
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     USERNAME_FIELD = 'email'
 
     def get_username(self):
