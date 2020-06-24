b2f03ced221c552d8577419d4faf2eff60892c0b
Natchapon Santiphiboon <winingame@hotmail.com>
2020-06-24 14:34:38 +0700

    Change settings.py to serve media files
    + urls.py

--- sop/views_post.py
+++ sop/views_post.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 
@@ -6,8 +7,9 @@ from sop.models import Post
 from sop.serializers import PostSerializer
 
 
-class PostViewSet(viewsets.GenericViewSet):
+class PostViewSet(viewsets.GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
     parser_classes = (MultiPartParser,)
+
