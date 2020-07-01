bd647d4482c51166bb733a39c211c81e5ac55f65
kemtis <60010107@kmitl.ac.th>
2020-07-01 13:35:44 +0700

    gitflow-feature-stash: group_groupAPI

--- grouplearning/permissions.py
+++ grouplearning/permissions.py
@@ -6,6 +6,12 @@ class IsCreatorUser(BasePermission):
     Allows access only to creator and admin users.
     """
 
+    def has_permission(self, request, view):
+        if request.user and request.user.is_authenticated:
+            return bool(request.user.sop_is_creator or request.user.is_staff)
+        else:
+            return False
+
 
 def get_permissions_multi(self):
     for p in self.permissions:
