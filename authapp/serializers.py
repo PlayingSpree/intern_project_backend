f945ccb06738547e3911d9f9893e3f1cf216d6d5
kemtis <60010107@kmitl.ac.th>
2020-07-09 19:12:07 +0700

    User image can upload.

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,9 +1,43 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from authapp.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.db import IntegrityError, transaction
+from djoser.conf import settings
+from django.core import exceptions as django_exceptions
+
 
 class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+    class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name', 'phone')
+        fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name', 'phone', 'image')
          
+    def validate(self, attrs):
+        user = User(**attrs)
+        password = attrs.get("password")
+
+        try:
+            validate_password(password, user)
+        except django_exceptions.ValidationError as e:
+            serializer_error = serializers.as_serializer_error(e)
+            raise serializers.ValidationError(
+                {"password": serializer_error["non_field_errors"]}
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        try:
+            user = self.perform_create(validated_data)
+        except IntegrityError:
+            self.fail("cannot_create_user")
+
+        return user
+
+    def perform_create(self, validated_data):
+        with transaction.atomic():
+            user = User.objects.create_user(**validated_data)
+            if settings.SEND_ACTIVATION_EMAIL:
+                user.is_active = False
+                user.save(update_fields=["is_active"])
+        return user
