3a7df9d1c84a6ed146e284ca86fa98fa85727140
Natchapon Santiphiboon <winingame@hotmail.com>
2020-07-10 16:40:27 +0700

    Fix member/course url

--- grouplearning/views_group.py
+++ grouplearning/views_group.py
@@ -1,18 +1,22 @@
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
+from authapp.serializers import UserDataSerializer
 from grouplearning.models import Group
 from grouplearning.permissions import get_permissions_multi
 from grouplearning.serializers import GroupSerializer
+from sop.serializers import CourseSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permissions = [
-        (['list', 'retrieve'], [IsAuthenticated]),
+        (['list', 'retrieve', 'member', 'course'], [IsAuthenticated]),
         (['create', 'update', 'partial_update', 'destroy'], [IsAdminUser])
     ]
     parser_classes = (MultiPartParser,)
@@ -26,3 +30,15 @@ class GroupViewSet(viewsets.ModelViewSet):
         serializer.save(creator_id=request.user)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=True)
+    def member(self, request, pk=None):
+        group = get_object_or_404(Group.objects.filter(id=pk))
+        serializer = UserDataSerializer(group.user_joined, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True)
+    def course(self, request, pk=None):
+        group = get_object_or_404(Group.objects.filter(id=pk))
+        serializer = CourseSerializer(group.courses, many=True)
+        return Response(serializer.data)
