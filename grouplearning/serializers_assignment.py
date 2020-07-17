439cd0822e73d4d4b987d454c5d1c4a6c952aeea
Natchapon Santiphiboon <winingame@hotmail.com>
2020-07-17 22:56:45 +0700

    Get Assignment include member count

--- grouplearning/serializers_assignment.py
+++ grouplearning/serializers_assignment.py
@@ -4,14 +4,29 @@ from djoser.serializers import UserSerializer
 from rest_framework import serializers
 
 from authapp.serializers import UserDataSerializer
-from .models import Assignment, AssignmentFile, AssignmentWorkFile, AssignmentWork
-
+from .models import Assignment, AssignmentFile, AssignmentWorkFile, AssignmentWork, Group
 
 # create group
+from .serializers import GroupSerializer
+
+
+class GroupAssignmentSerializer(serializers.ModelSerializer):
+    member_count = serializers.SerializerMethodField(read_only=True)
+
+    def get_member_count(self, obj):
+        return obj.user_joined.count()
+
+    class Meta:
+        model = Group
+        fields = ['id', 'member_count']
+        read_only_fields = ['id']
+
+
 class AssignmentSerializer(serializers.ModelSerializer):
     admin = UserDataSerializer(source='admin_id', read_only=True)
     assignment_files = serializers.SerializerMethodField(read_only=True)
     turn_in_count = serializers.SerializerMethodField(read_only=True)
+    group = GroupAssignmentSerializer(source='group_id', read_only=True)
 
     def get_assignment_files(self, obj):
         serializer = AssignmentFileSerializer(AssignmentFile.objects.filter(assignment_id=obj.id), many=True,
@@ -23,9 +38,10 @@ class AssignmentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Assignment
-        fields = ['id', 'group_id', 'admin', 'assignment_files', 'name', 'description', 'turn_in_count', 'date_created',
-                  'date_modified', 'due_date', 'allow_file']
+        fields = ['id', 'group_id', 'group', 'admin', 'assignment_files', 'name', 'description', 'turn_in_count',
+                  'date_created', 'date_modified', 'due_date', 'allow_file']
         read_only_fields = ['id', 'admin', 'date_created', 'date_modified']
+        extra_kwargs = {'group_id': {'write_only': True}}
 
 
 class AssignmentFileSerializer(serializers.ModelSerializer):
